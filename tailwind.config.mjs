024f30bc002c2107f63d376b0006c8a2fab94bea
BinderGerman <bindergerman@gmail.com>
2024-11-24 13:49:29 -0300

    Created Nabvar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: "#ff9000",
+        bgColor: "#1e242d",
+        seconBgColor: "#323946",
+        whiteColor: "#ffffff",
+        disabledColor: "#fff3",
+      },
+    },
+  },
+  plugins: [],
+};
