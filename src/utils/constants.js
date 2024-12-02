dd5e159decc5840dc768b3d0781b65170732b723
BinderGerman <bindergerman@gmail.com>
2024-12-01 21:07:57 -0300

    Btn experience - created experience section

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -53,4 +53,96 @@ export const data = {
       icon: "/wordpress.svg",
     },
   ],
+  professionalProfile: [
+    {
+      title: "Mi Experiencia",
+      description:
+        "Conocé un poco más sobre mí y mi experiencia en el trabajo.",
+    },
+    {
+      title: "Mi Educación",
+      description: "Conocé un poco más sobre mí y mi educación.",
+    },
+    {
+      title: "Mis Skills",
+      description: "Conocé un poco más sobre mis habilidades.",
+    },
+    {
+      title: "Sobre mi",
+      description: "Conocé un poco más sobre mi.",
+    },
+  ],
+
+  experiences: [
+    {
+      title: "Desarrollador Web",
+      company: "Agencia Siniestro",
+      summary:
+        "Responsable del soporte técnico y desarrollo web en entornos WordPress, enfocado en soluciones personalizadas y eficientes para mejorar el rendimiento y funcionalidad de los sitios de clientes.",
+      description: [
+        "Especialista en diagnóstico y resolución de problemas en sitios web WordPress, brindando soluciones innovadoras y adaptadas a las necesidades de cada cliente.",
+        "Amplia experiencia en la prueba, investigación y configuración de plugins dentro del ecosistema WordPress, logrando implementaciones robustas y optimizadas.",
+        "Diseño y maquetación de módulos y secciones con WP Bakery, Elementor y Gutenberg, priorizando la experiencia de usuario (UX) y un diseño visual atractivo.",
+        "Creación de entornos de prueba mediante clonación de sitios, garantizando cambios y actualizaciones sin riesgo para el sitio original.",
+        "Desarrollo de productos de soporte proactivo, enfocados en agregar valor a los sitios de los clientes mediante módulos de seguridad, optimización web y SEO.",
+        "Integración de herramientas de Google como Google Search Console y Google Merchant Center para mejorar la visibilidad y rendimiento web.",
+      ],
+      startDate: "Mayo 24",
+      endDate: "Actualidad",
+    },
+    {
+      title: "Desarrollador Full Stack",
+      company: "Nenes.io",
+      summary:
+        "Contribuí al desarrollo de una aplicación web (UI Trade) basada en Next.js, enfocada en generar soluciones de marketing personalizadas para pequeños negocios mediante inteligencia artificial.",
+      description: [
+        "Desarrollé componentes funcionales clave para la aplicación, optimizando la experiencia del usuario y mejorando la eficiencia general del sistema.",
+        "Participé en la implementación de funciones de IA para la creación automática de piezas gráficas y de marketing, ayudando a pequeñas empresas a mejorar su presencia en línea con contenido de alto impacto.",
+      ],
+      startDate: "Enero 23",
+      endDate: "Mayo 24",
+    },
+    {
+      title: "Desarrollador Web",
+      company: "Freelance",
+      summary:
+        "Como desarrollador freelance, he creado landing pages personalizadas para clientes de diversos sectores. Usando frameworks modernos como Astro y Next.js, he desarrollado soluciones rápidas, optimizadas y enfocadas en la conversión",
+      description: [
+        "Implementación de diseños responsivos que mejoran la experiencia del usuario en dispositivos móviles y de escritorio.",
+        "Optimización de la performance a través de técnicas de carga diferida (lazy loading) y minimización de recursos.",
+        "Integración de herramientas de SEO para mejorar la visibilidad de las páginas en buscadores.",
+        "Cada proyecto ha sido realizado con un enfoque en las necesidades específicas de los clientes, asegurando un balance entre rendimiento y estética para maximizar los resultados comerciales.",
+      ],
+      startDate: "Enero 23",
+      endDate: "Marzo 24",
+    },
+    {
+      title: "Desarrollador Full Stack",
+      company: "Tinkin",
+      summary:
+        "Desarrollé una aplicación web tipo Todo-List utilizando tecnologías modernas y aplicando buenas prácticas de desarrollo.",
+      description: [
+        "Implementé el backend con Nest.js, MongoDB como base de datos, y el frontend con Next.js, siguiendo el patrón de diseño MVC (Model View Controller) para una arquitectura escalable y organizada.",
+        "Aplicación de la metodología TDD (Test Driven Development), creando y ejecutando pruebas automatizadas con Jest para asegurar la calidad del código.",
+        "Colaboré en proyectos reales de la empresa, con un enfoque en el desarrollo frontend, contribuyendo al diseño y la implementación de interfaces de usuario.",
+        "Participé activamente en un entorno ágil bajo la metodología Scrum, asistiendo a reuniones diarias (dailies), contribuyendo a la planificación y ejecución de sprints, y participando en revisiones y retrospectivas. Utilización de Jira para la gestión de tareas y seguimiento del backlog mediante Kanban.",
+      ],
+      startDate: "Octubre 22",
+      endDate: "Enero 23",
+    },
+    {
+      title: "Desarrollador Full Stack",
+      company: "Tinkin",
+      summary:
+        "Desarrollé una aplicación web tipo Todo-List utilizando tecnologías modernas y aplicando buenas prácticas de desarrollo.",
+      description: [
+        "Implementé el backend con Nest.js, MongoDB como base de datos, y el frontend con Next.js, siguiendo el patrón de diseño MVC (Model View Controller) para una arquitectura escalable y organizada.",
+        "Aplicación de la metodología TDD (Test Driven Development), creando y ejecutando pruebas automatizadas con Jest para asegurar la calidad del código.",
+        "Colaboré en proyectos reales de la empresa, con un enfoque en el desarrollo frontend, contribuyendo al diseño y la implementación de interfaces de usuario.",
+        "Participé activamente en un entorno ágil bajo la metodología Scrum, asistiendo a reuniones diarias (dailies), contribuyendo a la planificación y ejecución de sprints, y participando en revisiones y retrospectivas. Utilización de Jira para la gestión de tareas y seguimiento del backlog mediante Kanban.",
+      ],
+      startDate: "Octubre 22",
+      endDate: "Enero 23",
+    },
+  ],
 };
--- src/utils/functions/getData.js
+++ src/utils/functions/getData.js
@@ -3,3 +3,11 @@ import { data } from "../constants";
 export const getTechnologies = () => {
   return data.technologies;
 };
+
+export const getProfessionalProfile = () => {
+  return data.professionalProfile;
+};
+
+export const getExperiences = () => {
+  return data.experiences;
+};
